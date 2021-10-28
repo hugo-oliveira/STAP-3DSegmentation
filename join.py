25c22070e494d05438dabde743014fadfd60c0fc
joseluiz432 <joseluizmacielp@gmail.com>
2021-10-28 17:33:56 -0300

    join up

--- join.py
+++ join.py
@@ -4,6 +4,8 @@ import sys
 from utils import *
 import os
 import re
+from sklearn import metrics
+
 
 def join_seg(prd_full, prd_cere, prd_vent, bbox_cere, bbox_vent):
     prd_full[:] = 0
@@ -71,10 +73,12 @@ lines_vent = csv_file_vent.readlines()
 lista_vent = [i.split() for i in lines_vent]
 dir_vent = {i[0]: np.array(i[1:], dtype='int') for i in lista_vent}
 
+prds_np = []
+labs_np = []
 for i, f in enumerate(files):
 
-
-    prd_full_path = os.path.join(prd_dir, f.replace('.nii.gz', '_prd.nii.gz'))
+    lab_path = os.path.join(lab_dir, f.replace('_prd', ''))
+    prd_full_path = os.path.join(prd_dir, f)
     prd_cere_path = os.path.join(prd_cere, f)
     prd_vent_path = os.path.join(prd_vent, f)
 
@@ -82,11 +86,30 @@ for i, f in enumerate(files):
     full = nib.load(prd_full_path)
     cere = nib.load(prd_cere_path)
     vent = nib.load(prd_vent_path)
+    lab = nib.load(lab_path)
 
+    lab_np = lab.get_fdata()
     full_np = full.get_fdata()
     cere_np = cere.get_fdata()
     vent_np = vent.get_fdata()
 
+
+    prds_all.extend(full.ravel().tolist())
+    labs_np.extend(lab_np.ravel().tolist())
+
     img_join = join_seg(full_np, cere_np, vent_np, dir_cere[f], dir_vent[f])
 
     save_nifti(img_join, out_lab_path)
+
+
+prds_np = np.asarray(prds_all).ravel()
+labs_np = np.asarray(lab_np).ravel()
+iou = metrics.jaccard_score(labs_np, prds_np, average='macro')
+dice = metrics.f1_score(labs_np, prds_np, average='macro')
+
+cm = metrics.confusion_matrix(labs_np, prds_np)
+print('--------------------------------------------------------------------')
+print('iou: %.4f, dice: %.4f' % ( iou, dice))
+print(cm)
+print('--------------------------------------------------------------------')
+sys.stdout.flush()
